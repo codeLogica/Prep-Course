1aaf5951cffd66c4f27a7ed6c74eb31ece8cb834
Ojkaal <medinarivasricardo@gmail.com>
2022-01-28 15:58:45 -0600

    Tarea Incompleta

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,25 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+
+  nombre[0] = `${nombre[0]}`.toUpperCase()
+
+  return nombre
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb()
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  cb(n1,n2)
+
+  return cb
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +30,22 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let sum;
+
+  numeros.forEach(n => {
+    sum = n[numeros.length-1] + n;
+  })
+
+  cb(sum)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(el => {
+    cb(el);
+  })
 }
 
 function map(array, cb) {
@@ -35,6 +53,14 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  const nuevoArr = [];
+
+  array.map(el => {
+    nuevoEl = cb(el);
+    nuevoArr.push(nuevoEl)
+  })
+
+  return nuevoArr
 }
 
 function filter(array) {
